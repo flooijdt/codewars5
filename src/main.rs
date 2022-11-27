53e3df32d9e23204b56fade366c17e82d6d5fc99
flooijdt <flooijdt@localhost.localdomain>
2022-11-27 10:06:28 -0300

    made it

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,34 @@ fn main() {
 }
 
 fn rgb(r: i32, g: i32, b: i32) -> String {
-    println!("{:?}", format!("{r:X?}"));
+    format!("{r:X?}");
+    println!("{:?}", r);
 
-    if r >= 255 {
-        let r: &str = "FF";
-    }
+    // if r >= 255 {
+    //     let r: &str = "FF";
+    // }
 
-    if g >= 255 {
-        let g: &str = "FF";
-    }
+    // if g >= 255 {
+    //     let g: &str = "FF";
+    // }
 
-    if b >= 255 {
-        let b: &str = "FF";
-    }
+    // if b >= 255 {
+    //     let b: &str = "FF";
+    // }
 
-    if r <= 0 {
-        let r: &str = "00";
-    }
+    // if r <= 0 {
+    //     let r: &str = "00";
+    // }
 
-    if g <= 0 {
-        let g: &str = "00";
-    }
+    // if g <= 0 {
+    //     let g: &str = "00";
+    // }
 
-    if b <= 0 {
-        let b: &str = "00";
-    }
+    // if b <= 0 {
+    //     let b: &str = "00";
+    // }
 
-    String::from("tifo")
+    // String::from(r).push_str(String::from(g));
+
+    String::from("whar")
 }
