869cab7738018ba651d2e35d8fba74d0dc3dc044
flooijdt <flooijdt@localhost.localdomain>
2022-11-27 09:50:57 -0300

    made it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
 fn main() {
     println!("Hello, world!");
+    rgb(422, 42, 42);
+}
+
+fn rgb(r: i32, g: i32, b: i32) -> String {
+    println!("{:?}", format!("{r:X?}"));
+
+    if r >= 255 {
+        r = "FF";
+    }
+
+    if g >= 255 {
+        g = "FF";
+    }
+
+    if b >= 255 {
+        b = "FF";
+    }
+
+    if r <= 0 {
+        r = "00";
+    }
+
+    if g <= 0 {
+        g = "00";
+    }
+
+    if b <= 0 {
+        b = "00";
+    }
+
+    String::from("tifo")
 }
