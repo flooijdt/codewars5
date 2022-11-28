aced50f37dcbd9f87cada99eeb63de2393b059a6
flooijdt <flooijdt@localhost.localdomain>
2022-11-28 07:33:57 -0300

    made it

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,40 @@ fn main() {
     println!("{}", rgb(4, 0, 257));
 }
 
-fn rgb(r: i32, g: i32, b: i32) -> String {
-    // format!("{r:2X?}");
-    //println!("{:02X}", r);
+fn rgb(mut r: i32, mut g: i32, mut b: i32) -> String {
+    format!("{r:2X?}");
+    println!("{:02X}", r);
 
-    if r <= 255 && r >= 0 {
+    if (0..=255).contains(&r) {
         let r: String = format!("{r:02X?}");
     } else {
         if r > 255 {
-            let r = String::from("FF");
+            r = 255;
         } else {
-            let r = String::from("00");
+            r = 0;
         }
     }
 
-    if g <= 255 && g >= 0 {
+    if (0..=255).contains(&g) {
         let g: String = format!("{g:02X?}");
     } else {
         if g > 255 {
-            let g = String::from("FF");
+            g = 255;
         } else {
-            let g = String::from("00");
+            g = 0;
         }
     }
 
-    if b <= 255 && b >= 0 {
+    if (0..=255).contains(&b) {
         let b: String = format!("{b:02X?}");
     } else {
         if b > 255 {
-            let b = String::from("FF");
+            b = 255;
+            println!("FFCACTA");
         } else {
-            let b = String::from("00");
+            b = 0;
         }
     }
-    println!("{:2X}{:2X}{:2X}", r, g, b);
-    format!("{r}{g}{b}")
+    println!("{:02X}{:02X}{:02X}", r, g, b);
+    format!("{r:02X}{g:02X}{b:02X}")
 }
